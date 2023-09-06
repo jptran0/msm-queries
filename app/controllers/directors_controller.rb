eb277b88a1f9a792431cb40338d2d31c84783edb
Jazmine Tran <117502378+jptran0@users.noreply.github.com>
2023-09-06 19:04:00 +0000

    added matching movies

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -5,8 +5,11 @@ class DirectorsController < ApplicationController
 
   def show
     # Params: {"id"=>"1"}
-    @director_id = params.fetch("id")
-    @director = Director.where({ id: @director_id}).first
+    director_id = params.fetch("id")
+    @director = Director.where({ id: director_id }).first
+
+    @matching_movies = Movie.where({ director_id: director_id })
+
     render template: "director_templates/show"
   end
 end
