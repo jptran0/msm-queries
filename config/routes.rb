1b04249a8d8ee7dfaba339241136fb0c83555029
Jazmine Tran <117502378+jptran0@users.noreply.github.com>
2023-09-06 18:14:13 +0000

    added routes for director and actor index

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   get("/", { :controller => "misc", :action => "homepage" })
+
+  get("/directors", { controller: "directors", action: "index" })
+
+  get("/actors", { controller: "actors", :action => "index"})
 end
