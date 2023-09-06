b25ec15a6370374a2fa50f04dd3cf2629f6a9a83
Jazmine Tran <117502378+jptran0@users.noreply.github.com>
2023-09-06 18:34:42 +0000

    route and view for director show page with params

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -2,4 +2,11 @@ class DirectorsController < ApplicationController
   def index
     render template: "director_templates/index"
   end
+
+  def show
+    # Params: {"id"=>"1"}
+    @director_id = params.fetch("id")
+    @director = Director.where({ id: @director_id}).first
+    render template: "director_templates/show"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get("/", { :controller => "misc", :action => "homepage" })
 
   get("/directors", { controller: "directors", action: "index" })
+  get("/directors/:id", { controller: "directors", action: "show" })
 
   get("/actors", { controller: "actors", :action => "index"})
 end
