fd61c7a818bee2fd82d3f5b136e343b61032b02b
Jazmine Tran <117502378+jptran0@users.noreply.github.com>
2023-09-07 19:43:30 +0000

    added actor show

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -3,4 +3,14 @@ class ActorsController < ApplicationController
     @actors = Actor.all
     render template: "actor_templates/index"
   end
+
+  def show
+    actor_id = params.fetch("id")
+
+    @actor = Actor.where({ id: actor_id }).first
+
+    @characters = Character.where({ actor_id: actor_id})
+
+    render template: "actor_templates/show"
+  end
 end
